9e4b8c1b3c8029eea2708394e855581594b4d0c0
My first test <mifalem@gmail.com>
2023-10-08 18:43:02 +0500

    programmadded

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
-}
+    public static void main(String[] args) {
+        int money = 0;
+        int payammount = 1489;
+
+        int bonus;
+        if (payammount >= 1000) {
+             bonus = payammount / 100;
+    } else {
+         bonus = 0;}
+        int check = money + payammount + bonus;
+        System.out.println(check);
+
+    }
+
+       }
